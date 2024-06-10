c1d013384ea5c45ca28448eec0815c2ea33d0fb5
Jack Carlton <j.carlton@uky.edu>
2024-06-10 18:34:37 -0400

    added quiet flag to environment setup script

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -12,9 +12,32 @@ script_directory=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 # Source utilities.sh from lib directory
 source "$script_directory/lib/utilities.sh"
 
+# Default value for quiet flag
+quiet=0
+
+# Function to print messages based on the quiet flag
+log() {
+    if [ $quiet -eq 0 ]; then
+        echo "$1"
+    fi
+}
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -q|--quiet)
+            quiet=1
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
 # Ensure the script is sourced
 if ! is_sourced_script; then
-    echo "This script must be sourced. Please run with 'source setup_env.sh' or '. setup_env.sh'."
+    log "This script must be sourced. Please run with 'source setup_env.sh' or '. setup_env.sh'."
     return 1
 fi
 
@@ -24,26 +47,26 @@ bin_directory=$(realpath "$script_directory/../bin")
 # Add the bin directory to PATH if it's not already included
 if ! is_in_path "$bin_directory"; then
     export PATH="$bin_directory:$PATH"
-    echo "Added $bin_directory to PATH."
+    log "Added $bin_directory to PATH."
 else
-    echo "$bin_directory is already in PATH."
+    log "$bin_directory is already in PATH."
 fi
 
 # Source the environment configuration file
 config_file="$script_directory/config/env_config.sh"
 if [[ -f "$config_file" ]]; then
     source "$config_file"
-    echo "Sourced environment configuration file: $config_file"
+    log "Sourced environment configuration file: $config_file"
 else
-    echo "Configuration file not found: $config_file"
+    log "Configuration file not found: $config_file"
     return 1
 fi
 
 # Convert the SYSTEM_MONITOR_CONFIG_FILE_PATH to an absolute path
 if [[ -n "$SYSTEM_MONITOR_CONFIG_FILE_PATH" ]]; then
     export SYSTEM_MONITOR_CONFIG_FILE_PATH=$(realpath "$SYSTEM_MONITOR_CONFIG_FILE_PATH")
-    echo "SYSTEM_MONITOR_CONFIG_FILE_PATH set to: $SYSTEM_MONITOR_CONFIG_FILE_PATH"
+    log "SYSTEM_MONITOR_CONFIG_FILE_PATH set to: $SYSTEM_MONITOR_CONFIG_FILE_PATH"
 else
-    echo "SYSTEM_MONITOR_CONFIG_FILE_PATH is not set in the configuration file."
+    log "SYSTEM_MONITOR_CONFIG_FILE_PATH is not set in the configuration file."
     return 1
 fi
